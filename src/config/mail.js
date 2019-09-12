86d6da48e5935388ebb0c9e4679900915358aa23
leonardoMoliveira <leonardo.m.oliveira98@gmail.com>
2019-09-11 21:16:05 -0300

    feat: sending email to MailTrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '78d7fcc657cdea',
+    pass: '995313509ad037',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
